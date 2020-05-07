47236aef205b7b6f6824bb50d4f9ee42636af01f
zara ahmadi <zatokey@gmail.com>
2020-05-07 19:57:39 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,7 +1,7 @@
 # Write your code here
-count = 0
-while count < 10 do
-  break if count == magic_exit_number
+count = 10
+while count > 10 do
+  break if count == 0
   puts "I am the #{count}, I love to count!" # Work
   count -= 1
 end
